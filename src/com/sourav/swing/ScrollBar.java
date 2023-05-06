1ee04680244cc66f313fb6633ca36275807d0107
Sourav Kurmi <100457899+tonystark5683@users.noreply.github.com>
2023-05-06 17:43:33 +0530

    Add files via upload

---
+++ src/com/sourav/swing/ScrollBar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sourav.swing;
+
+/**
+ *
+ * @author Procastinator
+ */
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JScrollBar;
+
+public class ScrollBar extends JScrollBar {
+
+    public ScrollBar() {
+        setUI(new ModernScrollBarUI());
+        setPreferredSize(new Dimension(5, 5));
+        setBackground(new Color(242, 242, 242));
+        setUnitIncrement(20);
+    }
+}
